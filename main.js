dfc39c136cf16015301fe2757e6fb07feee2c5f7
George Mathews <george@georgemathe.ws>
2017-11-19 23:25:36 -0800

    Added constants and documentation on next steps

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 // Configuration
 const frequency = 600; // Tone frequency
+const desiredWPM = 4; // Desired Words Per Minute
+
+// End Configuration
+
+const dotLength = 1200 / desiredWPM; // Milliseconds
+// Since these are standard to morse code, don't change these
+const dashLength = dotLength * 3;
+const elementGap = dotLength; // The gap between the dots and dashes in a character
+const minLetterGap = dotLength * 3; // Various teaching methods have you make these longer
+const minWordGap = dotLength * 7;
 
 // Setup a tone generator
 const audioContext = new (window.AudioContext || window.webkitAudioContext)();
@@ -59,6 +69,16 @@ function filterKey( key ){
     return key == 'Tab';
 
 }
+
+// Used to calculate Words Per Minute from our current dot size
+function calcWPM( averageDotSize ){
+    // averageDotSize is in milliseconds
+    return 1200 / averageDotSize;
+}
+
+// TODO: given some n elements, determine the size of a dot
+// TODO: given a element of length k, determine if it is a dash or dot.
+// TODO: given a gap of length k, determine if it is a letter, element, or word gap
 
 // On while key is down make a tone
 window.addEventListener( 'keydown', function( downEvent ){
