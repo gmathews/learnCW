69b7500094fda242c9d66d614f5f83dce46c4eb1
George Mathews <george@kungfufactory.com>
2017-12-04 19:42:52 -0800

    Allow us to tweak WPM

--- CWTranslate.js
+++ CWTranslate.js
@@ -1,16 +1,8 @@
 // Configuration
-const desiredWPM = 4; // Desired Words Per Minute
 const marginOfError = 0.25; // 10% margin of error to still count elements as dots
 const numberOfElementsToStore = 5; // Number of elements to calculate average
 
 // End Configuration
-
-const dotLength = 1200 / desiredWPM; // Milliseconds
-// Since these are standard to morse code, don't change these
-const dashLength = dotLength * 3;
-const elementGap = dotLength; // The gap between the dots and dashes in a character
-const minLetterGap = dotLength * 3; // Various teaching methods have you make these longer
-const minWordGap = dotLength * 7;
 
 const gapTypes = {
     INVALID: 0,
@@ -24,14 +16,26 @@ const gapTypes = {
 class CWTranslator{
 
     constructor(){
+        this.WPM = 5;
+    }
+
+    set WPM( wpm ){
+        this.desiredWPM = wpm; // Desired Words Per Minute
+        this.dotLength = 1200 / this.desiredWPM; // Milliseconds
+        // Since these are standard to morse code, don't change these
+        this.dashLength = this.dotLength * 3;
+        this.elementGap = this.dotLength; // The gap between the dots and dashes in a character
+        this.minLetterGap = this.dotLength * 3; // Various teaching methods have you make these longer
+        this.minWordGap = this.dotLength * 7;
+
         // This is the last n elements
-        this.elements = [ dotLength ]; // Start with our target value
+        this.elements = [ this.dotLength ]; // Start with our target value
         // Init our average dot size
         this._updateAverageDotSize();
     }
 
     // Used to calculate Words Per Minute from our current dot size
-    calcWPM(){
+    get WPM(){
         // averageDotSize is in milliseconds
         return 1200 / this.averageDotSize;
     }
@@ -76,48 +80,48 @@ class CWTranslator{
     isDash( element ){
         const marginOfErrorMs = marginOfError * this.averageDotSize;
         // Ratio of dot length to dash length
-        const dashDotDiff = this.averageDotSize * ( dashLength / dotLength );
+        const dashDotDiff = this.averageDotSize * ( this.dashLength / this.dotLength );
         return CWTranslator._betweenMarginOfError( element, dashDotDiff, marginOfErrorMs );
     }
     // [0=>1] Accepts element length in ms
     dashPercentage( element ){
         // Ratio of dot length to dash length
-        const dashDotDiff = this.averageDotSize * ( dashLength / dotLength );
+        const dashDotDiff = this.averageDotSize * ( this.dashLength / this.dotLength );
         return Math.max( 0, Math.min( 1.0, ( element ) / dashDotDiff ) );
     }
     // [0=>1] A dash is 84% of this bar. Accepts element length in ms.
     overflowPercentage( element ){
-        const overflowDotDiff = this.averageDotSize * ( dashLength / dotLength );
+        const overflowDotDiff = this.averageDotSize * ( this.dashLength / this.dotLength );
         return Math.min( 1.0, ( 0.84 * element ) / overflowDotDiff );
     }
 
     // [0=>1] length in ms.
     gapElementPercentage( space ){
-        const gapDotDiff = this.averageDotSize * ( elementGap / dotLength );
+        const gapDotDiff = this.averageDotSize * ( this.elementGap / this.dotLength );
         return Math.min( 1.0, space / gapDotDiff );
     }
     // [0=>1] length in ms.
     gapLetterPercentage( space ){
-        const letterDotDiff = this.averageDotSize * ( minLetterGap / dotLength );
+        const letterDotDiff = this.averageDotSize * ( this.minLetterGap / this.dotLength );
         return Math.min( 1.0, space / letterDotDiff );
     }
     // [0=>1] length in ms.
     gapWordPercentage( space ){
-        const wordDotDiff = this.averageDotSize * ( minWordGap / dotLength );
+        const wordDotDiff = this.averageDotSize * ( this.minWordGap / this.dotLength );
         return Math.min( 1.0, space / wordDotDiff );
     }
     // [0=>1] length in ms. [0-98] is word % in this percentage
     gapOverflowPercentage( space ){
-        const overflowDotDiff = this.averageDotSize * ( ( minWordGap ) / dotLength );
+        const overflowDotDiff = this.averageDotSize * ( ( this.minWordGap ) / this.dotLength );
         return Math.min( 1.0, ( 0.98 * space ) / overflowDotDiff );
     }
 
     // Given a gap of length k, determine if it is a letter, element, or word gap
     gapType( space ){
         // Ratio of dot length to gap length
-        const gapDotDiff = this.averageDotSize * ( elementGap / dotLength );
-        const LetterDotDiff = this.averageDotSize * ( minLetterGap / dotLength );
-        const WordDotDiff = this.averageDotSize * ( minWordGap / dotLength );
+        const gapDotDiff = this.averageDotSize * ( this.elementGap / this.dotLength );
+        const LetterDotDiff = this.averageDotSize * ( this.minLetterGap / this.dotLength );
+        const WordDotDiff = this.averageDotSize * ( this.minWordGap / this.dotLength );
         const marginOfErrorMs = marginOfError * this.averageDotSize;
 
         if( CWTranslator._betweenMarginOfError( space, gapDotDiff, marginOfErrorMs ) ){
